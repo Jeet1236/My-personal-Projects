98bf4bbb0bdacfe68e0b282f231d678a37f14727
Jeet Patel <bharatipatel105@gmail.com>
2023-05-07 18:34:32 -0400

    level 3 encryption. Adding env file for the security of the data.

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,4 +1,5 @@
 // jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -17,9 +18,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "HelloHowAreYouDoingInLifeDon'tGiveUp";
+
 userSchema.plugin(encrypt,
-    { secret: secret, encryptedFields: ["password"] });
+    { secret: process.env.SECRET, encryptedFields: ["password"] });
 const User = new mongoose.model("User", userSchema);
 app.get("/", function (req, res) {
     res.render("home");
