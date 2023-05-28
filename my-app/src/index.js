e4509e84a8632979d02c765e4b05eba9425af6b6
Jeet Patel <bharatipatel105@gmail.com>
2023-05-28 15:44:45 -0400

    Keeper App created using React JS

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(document.getElementById('root'));
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
 
 
