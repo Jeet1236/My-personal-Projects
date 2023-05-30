402cb570d568c0ab0f1a44a9dfb7df76438c74d0
Jeet Patel <bharatipatel105@gmail.com>
2023-05-29 20:57:21 -0400

    Adding Card file

--- React-Props/src/components/App.jsx
+++ React-Props/src/components/App.jsx
@@ -1,24 +1,5 @@
 import React from "react";
-function Card(props) {
-  return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">{props.name}</h2>
-          <img
-            src={props.imgURL}
-            alt="avatar_img" className="circle-img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+import Card from "./Card";
 function App() {
   return <div>
     <Card name="Beyonce"
